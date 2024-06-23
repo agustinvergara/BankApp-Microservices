d62f37719c6a61f16a602f45b2d9cc0494dce3d0
agustinvergara <agustin.vergara.ruiz@gmail.com>
2024-06-22 22:54:01 -0500

    :tada:INIT: Creating Spring Projects

---
+++ currents/src/main/java/com/current_account/currents/CurrentsApplication.java
@@ -0,0 +1,13 @@
+package com.current_account.currents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentsApplication.class, args);
+	}
+
+}
