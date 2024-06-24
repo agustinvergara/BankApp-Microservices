f3a58b2f1349fbd49f6ac0ffac46baf325e86b3f
agustinvergara <agustin.vergara.ruiz@gmail.com>
2024-06-23 23:53:43 -0500

    :wrench: CONFIG: working in config files

--- deposits/src/main/java/com/deposit_account/deposits/FeingClients/CurrentAccountService.java
+++ deposits/src/main/java/com/deposit_account/deposits/FeingClients/CurrentAccountService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
-@FeignClient(name = "CurrentsAccountService", url = "http://localhost:8081")
+@FeignClient(name = "CurrentsAccountService", url = "http://localhost:8083")
 public interface CurrentAccountService {
     @GetMapping(value = "/current")
     ResponseDTO<List<CurrentAccount>> getsCurrentsAccounts();
--- deposits/src/main/java/com/deposit_account/deposits/FeingClients/SavingAccountService.java
+++ deposits/src/main/java/com/deposit_account/deposits/FeingClients/SavingAccountService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "SavingAccountService", url = "http://localhost:8080")
+@FeignClient(name = "SavingAccountService", url = "http://localhost:8082")
 public interface SavingAccountService {
     @GetMapping(value = "/saving")
     ResponseDTO<List<SavingAccount>> getSavingAccounts();
